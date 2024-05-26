8fa247b8224c7ef550e63a29501a6be39ad16df5
Marten <derapfelesser@aol.de>
2024-05-26 16:21:13 +0200

    run coq_nvim on startup

--- lua/marten/lazy.lua
+++ lua/marten/lazy.lua
@@ -10,6 +10,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
+vim.g.coq_settings = {
+  auto_start = 'shut-up',
+}
 
 require('lazy').setup {
   { import = 'marten.plugins' },
