ec324edfde02c032f0acba8ca5cd2c87d4622861
Marten <derapfelesser@aol.de>
2024-06-06 12:11:06 +0200

    create keymaps for dap and install debug adapters with mason

--- lua/marten/plugins/dap/nvim-dap.lua
+++ lua/marten/plugins/dap/nvim-dap.lua
@@ -1,12 +1,12 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    'rcarriga/nvim-dap-ui',
-    'nvim-neotest/nvim-nio',
-    'theHamsta/nvim-dap-virtual-text',
-    'nvim-telescope/telescope-dap.nvim',
-
-    'leoluz/nvim-dap-go',
+    'rcarriga/nvim-dap-ui', -- ui when debugging
+    'nvim-neotest/nvim-nio', -- needed for nvim-dap ui
+    'theHamsta/nvim-dap-virtual-text', -- live evaluation of variables
+    'nvim-telescope/telescope-dap.nvim', -- telescope features for dap
+    'leoluz/nvim-dap-go', -- easy nvim-dap config for go with neat additional features
+    'jay-babu/mason-nvim-dap.nvim', -- use mason to install debug adapters
   },
 
   config = function()
@@ -14,26 +14,33 @@ return {
     local ui = require 'dapui'
     local dap_go = require 'dap-go'
     local virtual_text = require 'nvim-dap-virtual-text'
-    ui.setup()
-    virtual_text.setup()
+    local mason_dap = require 'mason-nvim-dap'
 
+    mason_dap.setup {
+      ensure_installed = { 'delve' },
+    }
+    ui.setup()
+    dap_go.setup()
+    virtual_text.setup {}
     -- for golang
     -- you'll have to install delve with go install github.com/go-delve/delve/cmd/dlv@latest
-    dap_go.setup()
-    dap.adapters.go = {
-      type = 'executable',
-      command = 'node',
-      args = { os.getenv 'HOME' .. '/dev/golang/vscode-go/dist/debugAdapter.js' },
-    }
-    dap.configurations.go = {
-      {
-        type = 'go',
-        name = 'Debug',
-        request = 'launch',
-        showLog = false,
-        program = '${file}',
-        dlvToolPath = vim.fn.exepath 'dlv', -- Adjust to where delve is installed
-      },
-    }
+    local setup_go_debugging_keymaps = function()
+      vim.keymap.set(
+        'n',
+        '<leader>dt',
+        ':lua require("dap-go").debug_test()<CR>',
+        { desc = 'Debug closest method above cursor', buffer = true }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>dp',
+        ':lua require("dap-go").debug_last_test()<CR>',
+        { desc = 'Debug previous run test', buffer = true }
+      )
+    end
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'go',
+      callback = setup_go_debugging_keymaps,
+    })
   end,
 }
