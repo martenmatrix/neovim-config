876740934f8b083fe049b094c88d45cae3cd20f1
Marten <derapfelesser@aol.de>
2024-06-08 23:33:20 +0200

    add keymaps for resizing splits

--- lua/marten/core/keymaps.lua
+++ lua/marten/core/keymaps.lua
@@ -8,6 +8,12 @@ keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' }) 
 keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' }) -- make split windows equal width & height
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' }) -- close current split window
 
+-- could also multiply current window size with a factor https://vim.fandom.com/wiki/Resize_splits_more_quicklyj
+keymap.set('n', '<leader>s+v', '<cmd>vertical resize +10<CR>', { desc = 'Vertically increase size of current split' })
+keymap.set('n', '<leader>s-v', '<cmd>vertical resize -10<CR>', { desc = 'Vertically decrease size of current split' })
+keymap.set('n', '<leader>s+h', '<cmd>resize +10<CR>', { desc = 'Horizontally increase size of current split' })
+keymap.set('n', '<leader>s-h', '<cmd>resize -10<CR>', { desc = 'Horizontally decrease size of current split' })
+
 keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
 keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
 keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
