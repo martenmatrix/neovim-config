e991afc47ae69e3ef77810e2cfbca302b7954c7f
Marten <derapfelesser@aol.de>
2024-06-23 17:51:14 +0200

    type gw to see waring/error in current line in a floating window

--- lua/marten/plugins/lsp/lsp-config.lua
+++ lua/marten/plugins/lsp/lsp-config.lua
@@ -30,6 +30,7 @@ return {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0, desc = 'Go to definition' })
       vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0, desc = 'Go to type definition' })
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0, desc = 'Go to implementation' })
+      vim.keymap.set('n', 'gw', vim.diagnostic.open_float, { buffer = 0, desc = 'Show warning/error in a floating window' })
     end
 
     mason_lspconfig.setup {
