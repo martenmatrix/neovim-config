968653e2075c8c10f796c40d30ff7fa061bf1a1b
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    add markdown preview

--- lua/marten/plugins/init.lua
+++ lua/marten/plugins/init.lua
@@ -17,4 +17,26 @@ return {
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    config = function()
+      vim.keymap.set(
+        'n',
+        '<leader>md',
+        '<cmd>MarkdownPreview<CR>',
+        { desc = 'Preview markdown in browser', buffer = true }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>mds',
+        '<cmd>MarkdownPreviewStop<CR>',
+        { desc = 'Stop markdown preview', buffer = true }
+      )
+    end,
+  },
 }
