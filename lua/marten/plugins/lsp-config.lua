5d6c7238abfe2ad2d34db6b29a5ce51b834aa77a
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    add language server for lua

---
+++ lua/marten/plugins/lsp-config.lua
@@ -0,0 +1,38 @@
+return {
+  'nvim-lspconfig',
+  config = function()
+    local lspconfig = require 'lspconfig'
+    local mason_lspconfig = require 'mason-lspconfig'
+    local cmp_nvim_lsp = require 'cmp_nvim_lsp'
+
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    mason_lspconfig.setup_handlers {
+      -- default handler
+      function(server_name)
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          on_attach = function()
+            -- will run inside every buffer when they first get attached to lsp
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0, desc = 'Show documentation for hovered text' })
+          end,
+        }
+      end,
+      ['lua_ls'] = function()
+        -- configure lua server (with special settings)
+        lspconfig['lua_ls'].setup {
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              -- make the language server recognize "vim" global
+              diagnostics = {
+                globals = { 'vim' },
+              },
+            },
+          },
+        }
+      end,
+    }
+  end,
+}
---
+++ lua/marten/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      local mason = require 'mason'
+      local mason_lspconfig = require 'mason-lspconfig'
+
+      mason.setup {}
+      mason_lspconfig.setup {
+        ensure_installed = { 'tsserver', 'html', 'cssls', 'lua_ls', 'gopls' },
+      }
+    end,
+  },
+}
