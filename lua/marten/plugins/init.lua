a9088b55697bb4caca1a32b2cde5a948641ef27b
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:41 +0200

    highlight current scope in purple

--- lua/marten/plugins/init.lua
+++ lua/marten/plugins/init.lua
@@ -28,7 +28,14 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     config = function()
-      require('ibl').setup()
+      local hooks = require 'ibl.hooks'
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'CurrentScope', { fg = '#fc5ef3' })
+      end)
+
+      require('ibl').setup {
+        scope = { highlight = { 'CurrentScope' } },
+      }
     end,
   },
 }
