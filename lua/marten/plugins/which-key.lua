008ec13c3c7498f371ee361d19410f6a16b1eae6
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    add keymapping to run which-key overview

--- lua/marten/plugins/which-key.lua
+++ lua/marten/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
+    vim.keymap.set('n', '<leader>h', '<cmd>WhichKey<CR>', { desc = 'Show which-key help' })
   end,
   opts = {
     popup_mappings = {
