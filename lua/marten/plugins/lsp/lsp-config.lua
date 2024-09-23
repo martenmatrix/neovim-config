566435d93aa75ef28f5e9b8a56a25b912d31abd3
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:41 +0200

    use lazydev for lua lsp

--- lua/marten/plugins/lsp/lsp-config.lua
+++ lua/marten/plugins/lsp/lsp-config.lua
@@ -3,6 +3,18 @@ return {
   dependencies = {
     'williamboman/mason-lspconfig.nvim',
     'hrsh7th/cmp-nvim-lsp',
+    {
+      'folke/lazydev.nvim',
+      ft = 'lua', -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        },
+      },
+    },
+    { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
   },
   config = function()
     -- vim.g.coq_settings needs to be set before lazys setup function is called, thus those settings are located in the init file
@@ -32,35 +44,17 @@ return {
         -- configure lua server (with special settings)
         lspconfig['lua_ls'].setup {
           capabilities = capabilities,
-          on_init = function(client)
-            local path = client.workspace_folders[1].name
-            if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-              return
-            end
-
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-              runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-              },
-              -- Make the server aware of Neovim runtime files
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  vim.env.VIMRUNTIME,
-                  -- Depending on the usage, you might want to add additional paths here.
-                  -- "${3rd}/luv/library"
-                  -- "${3rd}/busted/library",
-                },
-                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                -- library = vim.api.nvim_get_runtime_file("", true)
-              },
-            })
-          end,
-          settings = {
-            Lua = {},
-          },
+          root_dir = lspconfig.util.root_pattern(
+            'init.lua',
+            '.luarc.json',
+            '.luarc.jsonc',
+            '.luacheckrc',
+            '.stylua.toml',
+            'stylua.toml',
+            'selene.toml',
+            'selene.yml',
+            '.git'
+          ),
           on_attach = setup_keymaps,
         }
       end,
--- lua/marten/plugins/lsp/nvim-cmp.lua
+++ lua/marten/plugins/lsp/nvim-cmp.lua
@@ -6,7 +6,17 @@ return {
     'neovim/nvim-lspconfig',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
+    {
+      'hrsh7th/nvim-cmp',
+      -- optional completion source for require statements and module annotations
+      opts = function(_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, {
+          name = 'lazydev',
+          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+        })
+      end,
+    },
     'L3MON4D3/LuaSnip', -- snippet engine
     'saadparwaiz1/cmp_luasnip', -- for auto-completion
   },
