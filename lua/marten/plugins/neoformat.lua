9c6da03116e12a5493c0f8e5aa284d752be22ffa
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    remove settings which specify that stylua should read from stdin, reason the formatter broke

--- lua/marten/plugins/neoformat.lua
+++ lua/marten/plugins/neoformat.lua
@@ -5,12 +5,6 @@ return {
 		-- use stylua for lua formatting
 		vim.g.neoformat_enabled_lua = { "stylua" }
 
-		vim.g.neoformat_lua_stylua = {
-			exe = "stylua",
-			-- read from stdin
-			args = { "-s" },
-			stdin = true,
-		}
 		-- auto-format lua on save
 		-- create group, which clears itself after config-reload to prevent autocmd duplicates
 		local format_group = vim.api.nvim_create_augroup("FormatGroup", { clear = true })
@@ -18,7 +12,7 @@ return {
 			group = format_group,
 			pattern = "*.lua",
 			callback = function()
-				vim.cmd("Neoformat")
+				vim.cmd("Neoformat stylua")
 			end,
 		})
 	end,
