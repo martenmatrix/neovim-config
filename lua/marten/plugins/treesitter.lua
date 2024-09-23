c40803c9445fa0470138b2329361f3df65175e20
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    install treesitter for better syntax highlighting

---
+++ lua/marten/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        'json',
+        'javascript',
+        'typescript',
+        'tsx',
+        'yaml',
+        'html',
+        'css',
+        'prisma',
+        'markdown',
+        'markdown_inline',
+        'svelte',
+        'graphql',
+        'bash',
+        'lua',
+        'vim',
+        'dockerfile',
+        'gitignore',
+        'query',
+        'vimdoc',
+        'c',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      incremental_selection = { enable = true },
+      textobjects = { enable = true },
+      indent = {
+        enable = true,
+      },
+    }
+  end,
+}
