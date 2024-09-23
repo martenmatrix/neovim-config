1a929b437441fb979c1331cd6b446f2f97b4c690
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    change coloring of line numbers

--- lua/marten/plugins/init.lua
+++ lua/marten/plugins/init.lua
@@ -8,6 +8,16 @@ return {
     priority = 1000,
     opts = {},
     config = function()
+      require('tokyonight').setup {
+        on_highlights = function(hl)
+          hl.LineNrAbove = {
+            fg = '#6ab8ff',
+          }
+          hl.LineNrBelow = {
+            fg = '#ff6188',
+          }
+        end,
+      }
       vim.cmd [[colorscheme tokyonight]]
     end,
   },
