939f92fc074f7ef1c69ea48e19db0830292f25c2
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:40 +0200

    redo with U

--- lua/marten/core/keymaps.lua
+++ lua/marten/core/keymaps.lua
@@ -14,6 +14,8 @@ keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  
 keymap.set('n', '<leader>tN', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
 keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
 
+keymap.set('n', 'U', '<cmd>redo<CR>', { desc = 'Redo last change'}) -- Redo changes like an undo with U
+
 local go_to_tab = function()
   vim.ui.input({ prompt = 'Enter tab to navigate to: ' }, function(input)
     if input:match '%d*%+' then
