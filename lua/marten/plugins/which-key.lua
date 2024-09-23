d7d8941dbd155e84eabed15e1f7d2773134769ec
Marten <76761713+martenmatrix@users.noreply.github.com>
2024-09-23 10:17:41 +0200

    popup_mappings was renamed to keys

--- lua/marten/plugins/which-key.lua
+++ lua/marten/plugins/which-key.lua
@@ -7,7 +7,7 @@ return {
     vim.keymap.set('n', '<leader>hw', '<cmd>WhichKey<CR>', { desc = 'Show which-key help' })
   end,
   opts = {
-    popup_mappings = {
+    keys = {
       scroll_down = '<C-j>', -- binding to scroll down inside the popup
       scroll_up = '<C-k>', -- binding to scroll up inside the popup
     },
