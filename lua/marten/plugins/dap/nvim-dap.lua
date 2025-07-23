ffcaceb78d69c68db01bcbb54195b925203c8320
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2025-07-23 17:17:56 +0200

    stop using deprecated get_install_path

--- lua/marten/plugins/dap/nvim-dap.lua
+++ lua/marten/plugins/dap/nvim-dap.lua
@@ -43,7 +43,8 @@ return {
           command = 'node',
           -- 💀 Make sure to update this path to point to your installation
           args = {
-            require('mason-registry').get_package('js-debug-adapter'):get_install_path()
+
+            vim.fn.expand("$MASON/packages/js-debug-adapter")
               .. '/js-debug/src/dapDebugServer.js',
             '${port}',
           },
