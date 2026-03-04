1e6a2ec61f4bfcbaae6cb2e624289004988a8d00
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2026-03-04 11:23:55 +0100

    upgrade and use default keymaps for surrounding pair

--- lua/marten/plugins/nvim-surround.lua
+++ lua/marten/plugins/nvim-surround.lua
@@ -2,21 +2,4 @@ return {
   'kylechui/nvim-surround',
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
-  config = function()
-    require('nvim-surround').setup {
-      keymaps = {
-        insert = '<C-g>s',
-        insert_line = '<C-g>S',
-        normal = '<leader>sys',
-        normal_cur = '<leader>syss',
-        normal_line = '<leader>syS',
-        normal_cur_line = '<leader>sySS',
-        visual = '<leader>sS',
-        visual_line = '<leader>sgS',
-        delete = '<leader>sds',
-        change = '<leader>scs',
-        change_line = '<leader>scS',
-      },
-    }
-  end,
 }
