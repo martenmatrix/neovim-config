86da3a2f4cfcf52535ba444499cf8b2acd0498b9
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2026-03-04 13:08:16 +0100

    install typst preview and use mason as dependency

---
+++ lua/marten/plugins/typst-preview.lua
@@ -0,0 +1,6 @@
+return {
+  'chomosuke/typst-preview.nvim',
+  lazy = false, -- or ft = 'typst'
+  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  dependencies_bin = { ['tinymist'] = 'tinymist' }
+}
