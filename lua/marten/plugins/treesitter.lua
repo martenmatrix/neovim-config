c180d0fd1c202b383632e4ec1f76b4cf534fc054
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2026-04-10 16:14:58 +0200

    update telescope and include ensure instaled for treesitter

--- lua/marten/plugins/treesitter.lua
+++ lua/marten/plugins/treesitter.lua
@@ -1,5 +1,31 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  init = function()
+    require('nvim-treesitter').install {
+      'json',
+      'javascript',
+      'typescript',
+      'tsx',
+      'yaml',
+      'html',
+      'css',
+      'prisma',
+      'markdown',
+      'markdown_inline',
+      'svelte',
+      'graphql',
+      'bash',
+      'lua',
+      'vim',
+      'dockerfile',
+      'gitignore',
+      'query',
+      'vimdoc',
+      'c',
+      'styled', -- styled-components
+      'python',
+    }
+  end,
 }
