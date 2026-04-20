24c27a4bb7ca2e8d2c59ab7087aa7bbdcdbcfbfd
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2026-04-20 10:50:00 +0200

    add claude code

--- lua/marten/plugins/dap/nvim-dap.lua
+++ lua/marten/plugins/dap/nvim-dap.lua
@@ -64,6 +64,32 @@ return {
         end
       end
     end
+    if not dap.adapters['python'] then
+      dap.adapters.python = function(cb, config)
+        if config.request == 'attach' then
+          ---@diagnostic disable-next-line: undefined-field
+          local port = (config.connect or config).port
+          ---@diagnostic disable-next-line: undefined-field
+          local host = (config.connect or config).host or '127.0.0.1'
+          cb({
+            type = 'server',
+            port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+            host = host,
+            options = {
+              source_filetype = 'python',
+            },
+          })
+        else
+          cb({
+            type = 'executable',
+            command = vim.fn.expand '$MASON/bin/debugpy-adapter',
+            options = {
+              source_filetype = 'python',
+            },
+          })
+        end
+      end
+    end
     if not dap.configurations['python'] then
       dap.configurations.python = {
         {
